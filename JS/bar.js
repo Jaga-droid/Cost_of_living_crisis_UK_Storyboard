b076da4ff448403e19cfaddd802e5be569dd8567
Jaga-droid <pjagatheesh45@gmail.com>
2023-04-13 08:02:46 +0100

    final touches

--- JS/bar.js
+++ JS/bar.js
@@ -1,20 +1,19 @@
-// Set up margin, width, height values for the barchart_svg element.
-const margin = { top: 20, right: 20, bottom: 50, left: 70 };
-const width = 800 - margin.left - margin.right;
-const height = 400 - margin.top - margin.bottom;
+// Set up barchart_margin, width, height values for the barchart_svg element.
+const barchart_margin = { top: 20, right: 20, bottom: 50, left: 70 };
+const barchart_width = 800 - barchart_margin.left - barchart_margin.right;
+const barchart_height = 400 - barchart_margin.top - barchart_margin.bottom;
 
-// Create the SVG element with given dimensions and margins.
-const barchart_svg = d3.select("#chart")
+// Create the SVG element with given dimensions and barchart_margins.
+const barchart_svg = d3.select("#mybar")
   .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
+  .attr("width", barchart_width + barchart_margin.left + barchart_margin.right)
+  .attr("height", barchart_height + barchart_margin.top + barchart_margin.bottom)
   .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  .attr("transform", "translate(" + barchart_margin.left + "," + barchart_margin.top + ")");
 
 // Load the data from the CSV file using d3.csv() function.
-d3.csv("data.csv").then(function(data) {
-    // Use console.log() to verify that data has been loaded successfully.
-    console.log(data);
+d3.csv("https://raw.githubusercontent.com/Jaga-droid/Cost_of_living_crisis_UK_Storyboard/main/Resources/Analysis_Results/dispose.csv").then(function(data) {
+   
 
     // Map the data to arrays for the x-axis category names and the y-axis data values.
  
@@ -24,16 +23,16 @@ d3.csv("data.csv").then(function(data) {
     // Define scale functions for the x- and y-axes.
     const xScale = d3.scaleBand()
         .domain(categories)
-        .range([0, width])
+        .range([0, barchart_width])
         .padding(0.2);
 
     const yScale = d3.scaleLinear()
         .domain([0, d3.max(values)])
-        .range([height, 0]);
+        .range([barchart_height, 0]);
 
     // Add the x- and y-axes to the chart.
     barchart_svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
+      .attr("transform", "translate(0," + barchart_height + ")")
       .call(d3.axisBottom(xScale));
 
     barchart_svg.append("g")
@@ -48,6 +47,6 @@ d3.csv("data.csv").then(function(data) {
         .attr("x", d => xScale(d.Country))
         .attr("y", d => yScale(+d.disposable_income))
         .attr("width", xScale.bandwidth())
-        .attr("height", d => height - yScale(+d.disposable_income))
+        .attr("height", d => barchart_height - yScale(+d.disposable_income))
         .style("fill", (d, i) => d3.schemeSet2[i % 8]); // Use unique colors for each bar.
 });
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Cost_of_living_crisis_UK_Storyboard
 Penny Pinching Pandemic: The Reality of UK's Cost of Living 
+
+Data Source : data.gov.uk (Office for National Statistics)
